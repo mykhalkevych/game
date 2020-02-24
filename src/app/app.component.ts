5ea9a5609854d74ba5b16504b0b4c1f5da664bd9
Nazar <frontend.ukrinsoft@invitedesk.com>
2020-02-24 23:40:54 +0200

    add some code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'card-game';
+export class AppComponent implements AfterViewInit {
+  preloading = true;
+
+  constructor() {
+
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.preloading = false;
+    }, 2000);
+  }
 }
